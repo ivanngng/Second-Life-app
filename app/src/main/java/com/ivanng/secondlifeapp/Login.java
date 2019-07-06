9b73122442b18fe7ede75bbe08d60879d2215dc4
ivanng <ivan12535@gmail.com>
2019-07-07 01:36:47 +0800

    Login function

--- app/src/main/java/com/ivanng/secondlifeapp/Login.java
+++ app/src/main/java/com/ivanng/secondlifeapp/Login.java
@@ -1,14 +1,26 @@
 package com.ivanng.secondlifeapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
     private Button btnLogin, btnSignup;
+    private Button confirm_login;
+    private EditText login_email, login_password;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +30,67 @@ public class Login extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         btnSignup = findViewById(R.id.btnSignup);
 
+        confirm_login = findViewById(R.id.btn_confirmlogin);
+        login_email = findViewById(R.id.login_email);
+        login_password = findViewById(R.id.login_password);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
+            }
+        });
+
+
+        confirm_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = login_email.getText().toString().trim();
+                String password = login_password.getText().toString().trim();
+
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Login.this, "Please Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(Login.this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                if (password.length()<6) {
+
+                    Toast.makeText(Login.this, "Password too short", Toast.LENGTH_SHORT).show();
+                }
+
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                                } else {
+
+                                    Toast.makeText(Login.this, "Login Failed or User not Available", Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+                        });
+
+
+
             }
         });
     }
