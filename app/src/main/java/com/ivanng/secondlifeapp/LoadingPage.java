ca67f5126a125216defabaaa1e5773f45d0d3450
ivanng <ivan12535@gmail.com>
2019-07-12 00:49:57 +0800

    test loading page

---
+++ app/src/main/java/com/ivanng/secondlifeapp/LoadingPage.java
@@ -0,0 +1,20 @@
+package com.ivanng.secondlifeapp;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_page);
+
+        SystemClock.sleep(2000);
+        Intent loadingIntent = new Intent(LoadingPage.this,Login.class);
+        startActivity(loadingIntent);
+        finish();
+    }
+}
