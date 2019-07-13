777a11616d5f5b01fd2352529ad4ad939ea6d30c
ivanng <ivan12535@gmail.com>
2019-07-14 02:24:08 +0800

    SectionsPagerAdapter

--- app/src/main/java/com/ivanng/secondlifeapp/Home/HomeActivity.java
+++ app/src/main/java/com/ivanng/secondlifeapp/Home/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.ivanng.secondlifeapp.Home;
 
 import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -28,8 +30,25 @@ public class HomeActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate: starting.");
 
         setupBottomNavigationView();
-
+        setupViewPager();
     }
+
+    private void setupViewPager(){
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //index 0
+        adapter.addFragment(new HomeFragment()); //index 1
+        adapter.addFragment(new MessageFragment()); //index 2
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_menu_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_menu_camera);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_menu_camera);
+    }
+
 
     private void setupBottomNavigationView(){
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
