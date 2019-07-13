0104177eda19e42b166abce19e00199cab8fd76a
ivanng <ivan12535@gmail.com>
2019-07-14 00:01:24 +0800

    bottom navigation button

--- app/src/main/java/com/ivanng/secondlifeapp/HomeActivity.java
+++ app/src/main/java/com/ivanng/secondlifeapp/HomeActivity.java
@@ -2,9 +2,12 @@ package com.ivanng.secondlifeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 
 import com.firebase.ui.auth.AuthUI;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+import com.ivanng.secondlifeapp.Utils.BottomNavigationViewHelper;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,20 +15,23 @@ import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
-
-
-
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Log.d(TAG, "onCreate: starting.");
 
-
-
+        setupBottomNavigationView();
 
     }
 
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+    }
 
 }
