5c3b597caef30de54cd1a09ad2ee9bf56b95f511
ivanng <ivan12535@gmail.com>
2019-07-14 13:13:51 +0800

    setting

--- app/src/main/java/com/ivanng/secondlifeapp/Profile/ProfileAcitivity.java
+++ app/src/main/java/com/ivanng/secondlifeapp/Profile/ProfileAcitivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.ivanng.secondlifeapp.R;
@@ -22,11 +23,17 @@ public class ProfileAcitivity extends AppCompatActivity {
 
     private Context mContext = ProfileAcitivity.this;
 
+    private ProgressBar mProgressBar;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
+
+        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+        mProgressBar.setVisibility(View.GONE);
 
         setupBottomNavigationView();
         setupToolbar();
